ddfffb9c86f632f9c71c9e6c9207e6caddbf8069
Victor Lawrence <victor.lawrence@uga.edu>
2019-08-02 17:25:11 -0400

    reorganize

--- src/PixiStuff/boot.js
+++ src/PixiStuff/boot.js
@@ -2,22 +2,26 @@
 import { makeSimpleSprite } from './makeSimpleSprite'
 import { getDragHooksForSprite } from './getDragHooksForSprite'
 
-export const boot = ({ App, view }) => {
+export const boot = ({ App }) => {
   const regularSprite = makeSimpleSprite()
+
   regularSprite.width = 100
   regularSprite.height = 100
+
+  App.stage.addChild(regularSprite)
+
   const {
     dragStarted,
     dragEnded,
     dragMoved
   } = getDragHooksForSprite(regularSprite)
-  App.stage.addChild(regularSprite)
 
   dragStarted.add({
     dragStarted () {
       regularSprite.alpha = 0.5
     }
   })
+
   dragMoved.add({
     dragMoved (data) {
       const {
@@ -28,6 +32,7 @@ export const boot = ({ App, view }) => {
       regularSprite.y = dragY
     }
   })
+
   dragEnded.add({
     dragEnded () {
       regularSprite.alpha = 1.0

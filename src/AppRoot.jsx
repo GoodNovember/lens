7a4affa483f4bbea8f87a200c1ce8d062da5fce7
Victor Lawrence <victor.lawrence@uga.edu>
2019-08-08 17:41:07 -0400

    Added Three.js Renderer

--- src/AppRoot.jsx
+++ src/AppRoot.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { BabylonLayout } from './Layouts/BabylonLayout'
 import { RootLayout } from './Layouts/RootLayout'
 import { PixiLayout } from './Layouts/PixiLayout'
-
+import { ThreeLayout } from './Layouts/ThreeLayout'
 export const AppRoot = () => {
   return (
     <div>
-      <PixiLayout />
+      {/* <PixiLayout /> */}
+      <ThreeLayout />
     </div>
   )
 }

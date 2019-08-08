71529b7de8e9a3a6c0d8f1c3ca89105319814dc9
Victor Lawrence <victor.lawrence@uga.edu>
2019-08-08 18:34:24 -0400

    updated makeGetSet

--- src/makeGetSet.js
+++ src/makeGetSet.js
@@ -1,20 +1,21 @@
-export const makeGetSet = ({initialValue}) => {
-	let value = initialValue
-	let subscribers = new Set()
-	const getValue = () => value
-	const setValue = newValue => {
-		value = newValue
-		subscribers.forEach(sub => { sub(newValue) })
-	}
-	const subscribe = callback => {
-		if (subscribers.has(callback) === false) {
-			subscribers.add(callback)
-			callback(initialValue)
-		}
-		return () => {
-
-		}
-	}
-
-	return [getValue, setValue, subscribe]
+export const makeGetSet = ({ initialValue, skipInitialCall = false }) => {
+  let value = initialValue
+  let subscribers = new Set()
+  const getValue = () => value
+  const setValue = newValue => {
+    value = newValue
+    subscribers.forEach(sub => { sub(newValue) })
+  }
+  const subscribe = callback => {
+    if (subscribers.has(callback) === false) {
+      subscribers.add(callback)
+      if (skipInitialCall === false) {
+        callback(initialValue)
+      }
+    }
+    return () => {
+      subscribers.delete(callback)
+    }
+  }
+  return [getValue, setValue, subscribe]
 }

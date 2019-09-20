09b1b03557698caf3e557b624e7d914aa96c9c3b
Victor Lawrence <VictorLawrenceCode@gmail.com>
2019-09-20 17:21:37 -0400

    texas ranger has been temporariy dethroned

--- src/Wire/Diagram/targetDirectory/targetFile.target_parsed.js
+++ src/Wire/Diagram/targetDirectory/targetFile.target_parsed.js
@@ -1,10 +1,1 @@
-const log = (...stuff) => {
-  console.log(...stuff)
-}
-const log2 = (...stuff) => console.log(...stuff)
-function walrus (input, contact) {
-  return contact(input)
-}
-walrus(123, function (dog) {
-  log(dog)
-})
+console.log(hello)

639155875ea1819a5609bd1f9c6f5760bc5cac7e
Victor Lawrence <victor.lawrence@uga.edu>
2019-09-23 18:36:55 -0400

    A counter makes things a little clearer.

--- src/Wire/Diagram/texas-ranger.js
+++ src/Wire/Diagram/texas-ranger.js
@@ -14,38 +14,41 @@ const subPositionsByType = {
 
 export const TexasRanger = ast => {
   const output = []
+  let counter = 0
   walk(ast, {
     enter: function (node, parentNode, parentKey, parentIndex) {
       const isPartOfArray = typeof parentIndex === 'number'
       if (isPartOfArray) {
         if (parentNode) {
-          console.log(`enter A ${parentNode.type}.${parentKey}[${parentIndex}] = ${node.type}`)
+          console.log(`${counter} enter A ${parentNode.type}.${parentKey}[${parentIndex}] = ${node.type}`)
         } else {
-          console.log(`enter - ${node.type}`)
+          console.log(`${counter} enter - ${node.type}`)
         }
       } else {
         if (parentNode) {
-          console.log(`enter O ${parentNode.type}.${parentKey} = ${node.type}`)
+          console.log(`${counter} enter O ${parentNode.type}.${parentKey} = ${node.type}`)
         } else {
-          console.log(`enter P ${node.type}`)
+          console.log(`${counter} enter P ${node.type}`)
         }
       }
+      counter++
     },
     leave: function (node, parentNode, parentKey, parentIndex) {
       const isPartOfArray = typeof parentIndex === 'number'
       if (isPartOfArray) {
         if (parentNode) {
-          console.log(`leave A ${parentNode.type}.${parentKey}[${parentIndex}] = ${node.type}`)
+          console.log(`${counter} leave A ${parentNode.type}.${parentKey}[${parentIndex}] = ${node.type}`)
         } else {
-          console.log(`leave - ${node.type}`)
+          console.log(`${counter} leave - ${node.type}`)
         }
       } else {
         if (parentNode) {
-          console.log(`leave O ${parentNode.type}.${parentKey} = ${node.type}`)
+          console.log(`${counter} leave O ${parentNode.type}.${parentKey} = ${node.type}`)
         } else {
-          console.log(`leave P ${node.type}`)
+          console.log(`${counter} leave P ${node.type}`)
         }
       }
+      counter++
     }
   })
   return output
